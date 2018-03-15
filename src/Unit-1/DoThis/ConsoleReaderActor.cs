1e928187e88435b98886f973571b2caa4755ef75
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2018-03-15 13:50:37 +0100

    * ConsoleReaderActor.cs: WIP
    * Program.cs:
    * Messages.cs:
    * WinTail.csproj:
    * packages.config:
    * ValidationActor.cs:
    * ConsoleWriterActor.cs:

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,30 +10,48 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
+        public const string StartCommand = "start";
+        private readonly IActorRef _validationActor;
 
-        public ConsoleReaderActor(IActorRef consoleWriterActor)
+        public ConsoleReaderActor(IActorRef validationActor)
         {
-            _consoleWriterActor = consoleWriterActor;
+            _validationActor = validationActor;
         }
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
+                DoPrintInstructions();
+            GetAndValidateInput();
+        }
+
+        #region Internal methods
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message) && String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
+                // shut down the entire actor system (allows the process to exit)
                 Context.System.Terminate();
                 return;
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            // otherwise, just hand message off to validation actor
+            // (by telling its actor ref)
+            _validationActor.Tell(message);
         }
+        #endregion
 
     }
 }
